9330f65df107b6551c86f540e7f0a3b0ee38227b
Paweł Cendrowski <pcendrowski@sigma.ug.edu.pl>
2017-11-22 20:18:46 +0100

    Dodano szkielet strony

---
+++ lab3/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset=utf-8>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Aplikacje internetowe - Paweł Cendrowski</title>
+        <link rel=stylesheet media=all href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Advent+Pro&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <div id="title">
+                <h1><em>Wypożyczalnia samochodów - baza historii</em></h1>
+            </div>
+            
+            <div id="author_info">
+		<h1>Aplikacje internetowe LAB3 - Paweł Cendrowski</h1>
+            </div> 
+            
+           <div class="clearfix"></div>
+           
+           <nav>
+            <ul>
+                <li><a href="#">Wypożyczalnie</a></li>
+                <li><a href="#">Samochody</a></li>
+                <li><a href="#">Klienci</a></li>
+                
+            </ul>
+        </nav> 
+        </header>
+
+        <section id="main">
+            Tekst testowy<br>
+            Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>
+            Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>Tekst testowy<br>
+            
+            <footer>
+                <h1><u>To jest przykładowa strona internetowa. Dane są fikcyjne i przypadkowe !</u></h1>
+            
+            </footer>
+        </section>
+        
+        
+        
+        
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
