2de618ab0162699381aaf6cbc637e77e8b7b536e
Paweł Cendrowski <pcendrowski@sigma.ug.edu.pl>
2017-12-10 18:39:06 +0100

    Organizacja repo

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Aplikacje internetowe
 Zadania z przedmiotu 2017/18 studia. zaoczne
 
-1. [Lab1 - HTML5 i CSS3](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/lab1)
-2. [Lab2 - PHP, format JSON - "Sonda"](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/lab2)
-3. [Lab3 - CRUD - PHP + PDO](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/lab3)
-4. [Lab4 - CRUD - Yii framework](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/lab4)
+1. [Zad1 - HTML5](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/zad1_html5)
+2. [Zad2 - CSS3](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/zad2_css3)
+3. [Zad3 - PHP, format JSON - "Sonda"](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/zad3_php_json)
+4. [Zad4 - CRUD - PHP + PDO](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/zad4_pdo)
+5. [Zad5 - CRUD - Yii framework](https://github.com/pcendrowski/aplikacje_internetowe/tree/master/zad5_framework_yii)
--- zad4_pdo/index.php
+++ zad4_pdo/index.php
@@ -33,7 +33,7 @@
             </nav> 
                    
             <div class="name_table">
-                 <h1>Wypożyczalnie</h1>
+                 <h1>&nbsp;</h1>
             </div>
 	</header>
     
@@ -44,21 +44,8 @@
                     
 			
                     <?php
-                        require('wypozyczalnia.php');
                         require('config.php');
 
-                        $baza = new Baza($szbd,$host,$dbname,$port,$login,$haslo);
-
-                        if( isset($_GET['delete']) )      
-                          $baza->delete($_GET['delete']);
-                        elseif( isset($_POST['update']) ) 
-                          $baza->update($_POST['nazwa'],$_POST['ulica'],$_POST['nr_budynku'],$_POST['miasto'],$_POST['telefon'],$_POST['update']);
-                        elseif( isset($_POST['insert']) ) 
-                          $baza->insert($_POST['nazwa'],$_POST['ulica'],$_POST['nr_budynku'],$_POST['miasto'],$_POST['telefon']);
-                        elseif( isset($_GET['edit']) )    
-                          $baza->select_update($_GET['edit']);
-                        else 
-                          $baza->select_insert();
 
                     ?>
                                       
